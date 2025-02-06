b3b4568f18c7cb764fab0bbd9c07904722ae86a5
kwekamelia <kwekamelia@gmail.com>
2025-02-06 19:18:30 +0800

    healthcare provider class, medical record class, staff class

---
+++ src/humans/Staff.java
@@ -0,0 +1,27 @@
+package humans;
+
+import java.time.LocalDate;
+
+public class Staff extends Human {
+    private String staffId;
+    private String title;
+    private String department;
+
+    public Staff(String name, LocalDate dateOfBirth, String nricFin, MaritalStatus maritalStatus,
+                 ResidentialStatus residentialStatus, String nationality, String address, Contact contact,
+                 Sex sex, BloodType bloodType, boolean isVaccinated, String staffId, String title, String department) {
+
+        super(name, dateOfBirth, nricFin, maritalStatus, residentialStatus, nationality, address,
+                contact, sex, bloodType, isVaccinated);
+
+        this.staffId = staffId;
+        this.title = title;
+        this.department = department;
+    }
+
+    public String getStaffId() { return staffId; }
+
+    public String getTitle() { return title; }
+
+    public String getDepartment() { return department; }
+}
