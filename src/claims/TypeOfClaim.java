ade28a797e71a3c77e4ddc05319c53e179d6a955
kwekamelia <kwekamelia@gmail.com>
2025-02-07 16:09:51 +0800

    javadoc

--- src/claims/TypeOfClaim.java
+++ src/claims/TypeOfClaim.java
@@ -1,9 +1,20 @@
 package claims;
 
 /**
- * Defines different types of insurance claims (Inpatient, Outpatient).
+ * Defines different types of insurance claims.\
+ *
+ * <p>
+ *     A claim can have one of the following types:
+ * </p>
+ * <ul>
+ *     <li>{@link #INPATIENT} - A claim for medical treatment that requires hospital admission.</li>
+ *     <li>{@link #OUTPATIENT - A claim for medical treatment that does not require hospital admission.}</li>
+ * </ul>
  */
 
 public enum TypeOfClaim {
-    INPATIENT, OUTPATIENT
+    /** A claim for medical treatment that requires hospital admission. */
+    INPATIENT,
+    /** A claim for medical treatment that does not require hospital admission. */
+    OUTPATIENT
 }
