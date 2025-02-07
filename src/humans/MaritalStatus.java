bcb871297a62f71d6c02a7285b15b9b8c2b862c4
kwekamelia <kwekamelia@gmail.com>
2025-02-07 17:26:35 +0800

    javadoc marital status

--- src/humans/MaritalStatus.java
+++ src/humans/MaritalStatus.java
@@ -2,8 +2,20 @@ package humans;
 
 /**
  * Defines the marital status of a human.
+ * <p>
+ *     The marital status can be one of the following:
+ * </p>
+ * <ul>
+ *     <li>{@link #SINGLE} - The individual is not married.</li>
+ *     <li>{@link #MARRIED} - The individual is married.</li>
+ *     <li>{@link #DIVORCED} - The individual is divorced.</li>
+ *     <li>{@link #WIDOWED} - The individual has lost their spouse due to death.</li>
+ * </ul>
  */
 
 public enum MaritalStatus {
-    SINGLE, MARRIED, DIVORCED, WIDOWED
+    SINGLE, /** The individual is not married. */
+    MARRIED, /** The individual is married. */
+    DIVORCED, /** The individual is divorced. */
+    WIDOWED /** The individual has lost their spouse due to death. */
 }
