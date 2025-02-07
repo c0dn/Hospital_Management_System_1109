86f7bbf04e82ebedd3248e6a1e4436e5ac7418c7
kwekamelia <kwekamelia@gmail.com>
2025-02-07 16:27:05 +0800

    javadoc doctor

--- src/humans/Doctor.java
+++ src/humans/Doctor.java
@@ -4,11 +4,35 @@ import java.time.LocalDate;
 
 /**
  * Represents a doctor in the insurance system.
+ * <p>
+ *     A doctor is a specilaized type of {@link Staff} with an additional Medical Council Registration (MCR) number.
+ * </p>
  */
 
 public class Doctor extends Staff{
+
+    /** The Medical Council Registration (MCR) number of the doctor. */
     private String mcr;
 
+    /**
+     * Constructs a Doctor object with the specified details.
+     *
+     * @param name The name of the doctor.
+     * @param dateOfBirth The date of birth of the doctor.
+     * @param nricFin The NRIC/FIN (identification number) of the doctor.
+     * @param maritalStatus The marital status of the doctor.
+     * @param residentialStatus The residential status of the doctor.
+     * @param nationality The nationality of the doctor.
+     * @param address The residential address of the doctor.
+     * @param contact The contact details of the doctor.
+     * @param sex The sex of the doctor.
+     * @param bloodType The blood type of the doctor.
+     * @param isVaccinated The vaccination status of the doctor.
+     * @param staffId The staff ID of the doctor.
+     * @param title The title of the doctor (eg. Senior Doctor, Specialist).
+     * @param department The department the doctor belongs to.
+     * @param mcr The Medical Council Registration (MCR) number of the doctor.
+     */
     public Doctor(String name, LocalDate dateOfBirth, String nricFin, MaritalStatus maritalStatus,
                   ResidentialStatus residentialStatus, String nationality, String address, Contact contact,
                   Sex sex, BloodType bloodType, boolean isVaccinated, String staffId, String title, String department,
@@ -20,5 +44,10 @@ public class Doctor extends Staff{
         this.mcr = mcr;
     }
 
+    /**
+     * Retrieves the Medical Council Registration (MCR) number of the doctor.
+     *
+     * @return The MCR number.
+     */
     public String getmcr() { return mcr; }
 }
