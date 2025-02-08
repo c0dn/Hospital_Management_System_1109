eff0bc4cf900bb29e5707d7995bd698407c756a5
jerald-g <jeraldgohjg@gmail.com>
2025-02-08 20:54:53 +0800

    update

--- src/policy/AccidentInsurance.java
+++ src/policy/AccidentInsurance.java
@@ -6,25 +6,25 @@ import java.time.LocalDate;
 
 public class AccidentInsurance extends InsurancePolicy{
     private AccidentsType accidents;
-    private double dailyAllowance;
+    private double allowance;
 
     public AccidentInsurance(String policyId, String insuranceProvider, double deductible, InsuranceStatus insuranceStatus,
                              LocalDate startDate, LocalDate endDate, double coInsuranceRate, double premiumAmount, Patient policyHolder,
                              AccidentsType accidents, double dailyAllowance) {
         super(policyId, insuranceProvider, deductible, insuranceStatus, startDate, endDate, coInsuranceRate, premiumAmount, policyHolder);
         this.accidents = accidents;
-        this.dailyAllowance = dailyAllowance;
+        this.dailyAllowance = allowance;
     }
     public AccidentsType getAccidents() {
         return accidents;
     }
 
-    public double getDailyAllowance() {
-        return dailyAllowance;
+    public double getAllowance() {
+        return allowance;
     }
     public void displayPolicyDetails() {
         super.displayPolicyDetails();
         System.out.format("Covered Accident Type: %s%n", accidents);
-        System.out.format("Daily Allowance: $%.2f%n", dailyAllowance);
+        System.out.format("Daily Allowance: $%.2f%n", allowance);
     }
 }
