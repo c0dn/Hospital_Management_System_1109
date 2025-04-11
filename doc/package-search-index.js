daf7bbecd7fc3e9dfc695ebb916574264f906ba1
c0dn <19631981+c0dn@users.noreply.github.com>
2025-04-11 23:24:28 +0800

    Cleanup

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"billing"},{"l":"claims"},{"l":"humans"},{"l":"insurance"},{"l":"medical"},{"l":"policy"},{"l":"tests"},{"l":"utils"},{"l":"wards"}];updateSearchResults();
